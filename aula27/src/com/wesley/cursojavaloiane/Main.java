f1b8c50cb811639f0f366550ff7b96bb2969e7d5
Wesley Menezes Januário <wesleyssjanuario@gmail.com>
2020-06-02 17:34:27 -0300

    aula27

---
+++ aula27/src/com/wesley/cursojavaloiane/Main.java
@@ -0,0 +1,10 @@
+package com.wesley.cursojavaloiane;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Teste");
+    }
+}
