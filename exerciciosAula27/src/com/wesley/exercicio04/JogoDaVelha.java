b022a6fed7776bc48f8b311bf8dc8048a89973d7
Wesley Menezes Januário <wesleyssjanuario@gmail.com>
2020-06-08 13:51:50 -0300

    Exercicio 4

--- exerciciosAula27/src/com/wesley/exercicio04/JogoDaVelha.java
+++ exerciciosAula27/src/com/wesley/exercicio04/JogoDaVelha.java
@@ -1,4 +1,47 @@
 package com.wesley.exercicio04;
 
 public class JogoDaVelha {
+
+    char[][] jogoVelha = new char[3][3];
+    int jogada = 1;
+
+    boolean validarJogada(int linha, int coluna, char sinal){
+        if(jogoVelha[linha][coluna] == sinal || jogoVelha[linha][coluna] == 'O'){
+            return false;
+        }else { //joagada válida
+            jogoVelha[linha][coluna] = sinal;
+            jogada++;
+            return true;
+        }
+    }
+
+    void imprimirTabuleiro(){
+        for(int i = 0; i < jogoVelha.length; i++){
+            for(int j = 0; j < jogoVelha[i].length; j++){
+                System.out.println(jogoVelha[i][j] + " | ");
+            }
+            System.out.println();
+        }
+    }
+
+    boolean verificarGanhador(char sinal){
+        //Verifica Ganhador
+        if((jogoVelha[0][0] == sinal && jogoVelha[0][1] == sinal && jogoVelha[0][2] == sinal) ||
+           (jogoVelha[1][0] == sinal && jogoVelha[1][1] == sinal && jogoVelha[1][2] == sinal) ||
+           (jogoVelha[2][0] == sinal && jogoVelha[2][1] == sinal && jogoVelha[2][2] == sinal) ||
+           (jogoVelha[0][0] == sinal && jogoVelha[1][0] == sinal && jogoVelha[2][0] == sinal) ||
+           (jogoVelha[0][1] == sinal && jogoVelha[1][1] == sinal && jogoVelha[2][1] == sinal) ||
+           (jogoVelha[0][2] == sinal && jogoVelha[1][2] == sinal && jogoVelha[2][2] == sinal) ||
+           (jogoVelha[0][0] == sinal && jogoVelha[1][1] == sinal && jogoVelha[2][2] == sinal)){
+          return true;
+        }
+        return false;
+    }
+
+    boolean vezJogador1(){
+        if(jogada % 2 == 1){
+            return true;
+        }
+        return false;
+    }
 }
