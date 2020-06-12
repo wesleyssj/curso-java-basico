88ef25b77eaec5f647caa67acf8c4b579a61a43a
Wesley Menezes Januário <wesleyssjanuario@gmail.com>
2020-06-12 17:00:28 -0300

    Aula 33

---
+++ aula33/src/com/wesley/cursojavaloiane/TesteCalculadora.java
@@ -0,0 +1,14 @@
+package com.wesley.cursojavaloiane;
+
+public class TesteCalculadora {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        MinhaCalculadora calc = new MinhaCalculadora();
+
+        calc.soma(1,2);
+        calc.soma(1.0, 2.0);
+
+    }
+}
