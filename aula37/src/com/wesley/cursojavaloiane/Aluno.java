f87019ae6bb5e76a462126113a8a78052334e19f
Wesley Menezes Januário <wesleyssjanuario@gmail.com>
2020-06-25 08:23:26 -0300

    Aula 37

---
+++ aula37/src/com/wesley/cursojavaloiane/Aluno.java
@@ -0,0 +1,31 @@
+package com.wesley.cursojavaloiane;
+
+public class Aluno extends Pessoa{
+
+    private String curso;
+    private double[] notas;
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public double calcularMedia(){
+        return 0;
+    }
+
+    public boolean verificarAprovado(){
+        return true;
+    }
+}
---
+++ aula37/src/com/wesley/cursojavaloiane/Teste.java
@@ -0,0 +1,24 @@
+package com.wesley.cursojavaloiane;
+
+public class Teste {
+
+    public static void main(String[] args) {
+	// write your code here
+
+       /* Aluno aluno = new Aluno();
+
+        Professor professor = new Professor();
+
+        Pessoa pessoa = new Pessoa();
+
+        aluno.setNome("Maria");*/
+
+        Pessoa aluno = new Aluno();
+
+       // aluno.get
+
+        Pessoa professor = new Professor();
+
+        //professor.getSala
+    }
+}
