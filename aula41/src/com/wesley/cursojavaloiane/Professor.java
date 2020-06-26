00a3de23df36e5787fd05ca57724212c93165767
Wesley Menezes Januário <wesleyssjanuario@gmail.com>
2020-06-26 12:43:02 -0300

    Aula 41

---
+++ aula41/src/com/wesley/cursojavaloiane/Professor.java
@@ -0,0 +1,47 @@
+package com.wesley.cursojavaloiane;
+
+public class Professor extends Pessoa{
+
+    private double salario;
+    private String nomeCurso;
+
+    public double getSalario() {
+
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+
+        this.salario = salario;
+    }
+
+    public String getNomeCurso() {
+
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+
+        this.nomeCurso = nomeCurso;
+    }
+
+    public double calcularSalarioLiquido(){
+
+        return 0;
+    }
+
+    public String obterEtiquetaEnderecio(){
+
+        String s = "Endereço do Professor: ";
+        s += super.getEndereco();
+
+        return s;
+    }
+
+    @Override
+    public String imprimirEtiquetaEndereco() {
+        return null;
+    }
+
+
+}
---
+++ aula41/src/com/wesley/cursojavaloiane/Teste.java
@@ -0,0 +1,22 @@
+package com.wesley.cursojavaloiane;
+
+public class Teste {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //Pessoa pessoa = new Pessoa();
+        Pessoa aluno = new Aluno();
+        Pessoa professor = new Professor();
+
+        //pessoa.setEndereco("Rua1, num1");
+        aluno.setEndereco("Rua2, num2");
+        professor.setEndereco("Rua3, num3");
+
+        //System.out.println(aluno.obterEtiquetaEnderecio());
+        //System.out.println(professor.obterEtiquetaEnderecio());
+
+        aluno.imprimirEtiquetaEndereco();
+        professor.imprimirEtiquetaEndereco();
+    }
+}
