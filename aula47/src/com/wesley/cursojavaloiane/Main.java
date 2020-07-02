8b360e87cd5dbe0d99d0d65fbd1160d0dbc98e84
Wesley Menezes Januário <wesleyssjanuario@gmail.com>
2020-07-02 20:21:53 -0300

    Aula 47

---
+++ aula47/src/com/wesley/cursojavaloiane/Main.java
@@ -0,0 +1,22 @@
+package com.wesley.cursojavaloiane;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        try{
+            int[] vetor = new int[4];
+
+            System.out.println("Antes da exception");
+
+            vetor[5] = 10;
+
+            System.out.println("Esse texto não será impresso!");
+        } catch(ArrayIndexOutOfBoundsException exception){
+            System.out.println("Excecção ao acessar um índice do vetor que não existe");
+        }
+
+        System.out.println("Este texto será impresso após a exception");
+
+    }
+}
